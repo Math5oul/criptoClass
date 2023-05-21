c39199c8800b09d27e25986d363ed755dee5c9f5
Matheus Dias <matheus.silva.dias@g.globo>
2023-05-21 17:17:34 -0300

    Add a funçao de decifrar nas cifras aplicaveis

--- src/app/components/ceasear-cipher/ceasar-cipher.component.ts
+++ src/app/components/ceasear-cipher/ceasar-cipher.component.ts
@@ -9,10 +9,18 @@ export class CeasarCipherComponent {
   message: string = ''; // Variável para armazenar a mensagem de entrada
   shift: number = 0; // Variável para armazenar o valor do deslocamento
   encryptedMessage: string = ''; // Variável para armazenar a mensagem criptografada
+  decryptedMessage: string = ''; // Variável para armazenar a mensagem criptografada
 
   // Função para realizar a cifração
   cipher() {
     this.encryptedMessage = this.encryptMessage(this.message, this.shift);
+    this.decryptedMessage = ''
+  }
+
+  // Função para realizar a uncifração
+  decipher() {
+    this.decryptedMessage = this.decryptMessage(this.message, this.shift);
+    this.encryptedMessage = ''
   }
 
   // Função para criptografar a mensagem usando o algoritmo da cifra de César
@@ -44,5 +52,36 @@ export class CeasarCipherComponent {
     }
 
     return encryptedMessage; // Retorna a mensagem criptografada
+  }
+
+   // Função para descriptografar a mensagem usando o algoritmo da cifra de César
+   decryptMessage(message: string, shift: number): string {
+    let decryptedMessage = '';
+
+    for (let i = 0; i < message.length; i++) {
+      const char = message[i];
+
+      // Verifica se o caractere é uma letra (A-Z ou a-z)
+      if (char.match(/[a-zA-Z]/)) {
+        const charCode = message.charCodeAt(i);
+        let shiftedCharCode;
+
+        if (char.match(/[A-Z]/)) {
+          // Caractere maiúsculo
+          shiftedCharCode = ((charCode - 65 - shift + 26) % 26) + 65;
+        } else {
+          // Caractere minúsculo
+          shiftedCharCode = ((charCode - 97 - shift + 26) % 26) + 97;
+        }
+
+        // Converte o código do caractere deslocado de volta para um caractere e adiciona à mensagem descriptografada
+        decryptedMessage += String.fromCharCode(shiftedCharCode);
+      } else {
+        // Se o caractere não for uma letra, simplesmente adiciona à mensagem descriptografada
+        decryptedMessage += char;
+      }
+    }
+
+    return decryptedMessage; // Retorna a mensagem descriptografada
   }
 }
--- src/app/components/vigenere-cipher/vigenere-cipher.component.ts
+++ src/app/components/vigenere-cipher/vigenere-cipher.component.ts
@@ -9,8 +9,10 @@ export class VigenereCipherComponent {
   keyword: string = ''; // Variável para armazenar a palavra-chave
   plaintext: string = ''; // Variável para armazenar o texto simples
   ciphertext: string = ''; // Variável para armazenar o texto criptografado
+  decryptedText: string = ''; // Variável para armazenar o texto descriptografado
 
   cipher(): void {
+    this.decryptedText = '';
     const keywordLength = this.keyword.length;
 
     // Repete a palavra-chave para cobrir toda a extensão do texto simples
@@ -24,6 +26,25 @@ export class VigenereCipherComponent {
         const keywordCharCode = repeatedKeyword[index].toLowerCase().charCodeAt(0) - 97; // Converte o caractere da palavra-chave para o código de 0 a 25
         const encryptedCharCode = (charCode + keywordCharCode) % 26; // Aplica a cifra de Vigenère
         return String.fromCharCode(encryptedCharCode + 97); // Converte o código de volta para um caractere e adiciona ao texto criptografado
+      })
+      .join('');
+  }
+
+  decipher(): void {
+    this.ciphertext = '';
+    const keywordLength = this.keyword.length;
+
+    // Repete a palavra-chave para cobrir toda a extensão do texto criptografado
+    const repeatedKeyword = this.keyword.repeat(Math.ceil(this.plaintext.length / keywordLength)).substr(0, this.plaintext.length);
+
+    this.decryptedText = this.plaintext
+      .replace(/[^a-zA-Z]/g, '') // Remove caracteres não alfabéticos
+      .split('')
+      .map((char, index) => {
+        const charCode = char.toLowerCase().charCodeAt(0) - 97; // Converte o caractere para o código de 0 a 25
+        const keywordCharCode = repeatedKeyword[index].toLowerCase().charCodeAt(0) - 97; // Converte o caractere da palavra-chave para o código de 0 a 25
+        const decryptedCharCode = (charCode - keywordCharCode + 26) % 26; // Desfaz a cifra de Vigenère
+        return String.fromCharCode(decryptedCharCode + 97); // Converte o código de volta para um caractere e adiciona ao texto descriptografado
       })
       .join('');
   }
